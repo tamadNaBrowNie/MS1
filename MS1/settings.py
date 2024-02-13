4631a33d62b5676b6de6e999501396d7797f8175
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-02-13 21:22:48 +0800

    it just works

--- MS1/settings.py
+++ MS1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MS1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['.\\MS1_web\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ MS1_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Ms1WebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MS1_web'
