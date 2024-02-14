4507a4d2ad5111d9d0ea611c5b61bdaa245f5233
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-02-15 01:12:47 +0800

    working on rest api

--- MS1/urls.py
+++ MS1/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
   
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('MS1_web.urls'))
+    # path('admin/', admin.site.urls),
+    path('', include('MS1_web.urls'))
 ]
