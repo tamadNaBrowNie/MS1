26fa13db394ebb0a568689721353159eae677315
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-02-15 23:57:39 +0800

    final test

--- rest_api/views.py
+++ rest_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from MS1_web.models import user
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password,make_password
 from .serializer import UserSerializer
 # Create your views here.
 from rest_framework.parsers import JSONParser 
@@ -17,21 +17,25 @@ def LoginUser(request):
     bio =    """
     SELECT username, email,legal_name, pfp, phone FROM user WHERE user.username = %(name)s ; 
                  """
+    
     creds = request.query_params
     uname = request.query_params['username']
-    dat = user.objects.raw(cred,[uname])
-    print (dat)
-    # if not check_password(password = creds['pw'],encoded = dat['pw']):pass
-    #     return Response(status = 500)
+    # pw = user.pw.filter(username=uname)
+    dat = user.objects.filter(username=uname)
+    pw = dat[0].pw
+    # print (dat)
+    if not check_password(password = request.query_params['pw'],encoded = pw):
+        return Response(status = 500)
 
     # dat = MyUser.objects.raw(bio,creds)
 
-    return Response(status = 200)
+    return Response(pw,status = 200)
 @api_view(['POST'])
 def NewUser(request):
     # this is the query
-    
-    serial = UserSerializer(data = request.data)
+    data = request.data
+    data['pw'] = make_password(data['pw'])
+    serial = UserSerializer(data)
     if serial.is_valid():
         serial.save()
         return Response(serial.data,status=200)
