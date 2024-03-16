f0ee3b76b088a30f39257315c3cf04c6a15aed85
Shawn <shawn_aberin@dlsu.edu.ph>
2024-03-17 00:23:55 +0800

    login required to add rate limit

--- MS1_web/views.py
+++ MS1_web/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 from .models import user
 from .forms import UserForm,LoginForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def reg(request):
     form = UserForm()
     context = {'form':form}
     return render(request, 'reg.html',context)
+@login_required
 def login(request):
     form = LoginForm()
     context = {'form':form}
